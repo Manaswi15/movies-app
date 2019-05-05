a11619f92e255710b9b1d2a4d1f1503ec2adac89
unknown <sanama87@gmail.com>
2019-05-05 15:11:32 +0530

    Added initial Header component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/home/Home';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+//Movies App
+ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
